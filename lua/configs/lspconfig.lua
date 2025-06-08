f444e94d19f64cd6907093c04f9ce8506e773304
okparaa <iamokpara@gmail.com>
2025-06-08 15:25:11 +0100

    corrected diagnostics' :

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,8 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "prettier", "cssls", "tailwindcss", "ts_ls", "emmet_language_server", "eslint" }
+local servers = { 
+  "html", "prettier", "cssls", "tailwindcss", "ts_ls", "emmet_language_server", "eslint" 
+}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -26,10 +26,4 @@ return {
       filters = { dotfiles = true }
     },
   },
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    ft = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
-    opts = {},
-  }
 }
