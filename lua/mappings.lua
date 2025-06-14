591e3dac6e42fa4b8942248a42311c13ec641586
okparaa <iamokpara@gmail.com>
2025-06-14 06:50:12 +0100

    correct keymap save in insert mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,10 +12,21 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("i", "<C-s>", "<ESC>:w<CR>", { desc = "Save file in Insert mode" })
+
 local function wrap_quotes()
-  vim.cmd('normal! ciw"')
-  vim.api.nvim_put({vim.fn.getreg('"')}, 'c', true, true)
-  vim.cmd('normal! a"')
+  -- Get word under cursor (without surrounding non-word chars)
+  local word = vim.fn.expand('<cword>')
+  
+  -- Get start and end positions of the word
+  local start_col = vim.fn.matchstrpos(vim.fn.getline('.'), '\\<'..word..'\\>')[2]
+  local end_col = start_col + #word - 1
+  
+  -- Replace with quoted version
+  vim.api.nvim_buf_set_text(0, 
+    vim.fn.line('.')-1, start_col,
+    vim.fn.line('.')-1, end_col+1,
+    {'"' .. word .. '"'})
 end
 
 map("n", "<leader>q", wrap_quotes, { desc = "Wrap word in quotes" })
