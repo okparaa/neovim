d18938a9a16dd330c85a16b9750c28c4b9fa05f9
okpara ifeanyi <okparaa@gmail.com>
2025-10-06 17:39:05 +0100

    modified nvim tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -8,19 +8,31 @@ return {
 				side = "left",
 			},
 			filters = {
-				dotfiles = false,
+				dotfiles = true,
+				git_ignored = true,
 			},
 			git = {
 				enable = true,
-				ignore = false,
+				ignore = true,
 			},
 			renderer = {
 				highlight_git = true,
+				group_empty = true,
 				icons = {
 					show = {
 						git = true,
+						file = true,
+						folder = true,
+						folder_arrow = true,
 					},
 				},
+				root_folder_label = function(path)
+					local home = os.getenv("HOME")
+					if path:find(home, 1, true) == 1 then
+						return "~" .. path:sub(#home + 1)
+					end
+					return vim.fn.fnamemodify(path, ":t")
+				end,
 			},
 		})
 	end,
